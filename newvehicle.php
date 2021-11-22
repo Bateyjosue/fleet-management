96e186312281809c6eec01e04e88a2ec2f1d60e9
Bateyjosue <josuebatey19@gmail.com>
2021-11-22 12:31:23 +0200

    update string connection newvehicle

--- newvehicle.php
+++ newvehicle.php
@@ -2,7 +2,7 @@
 if (!isset($_SESSION)) {
     session_start();
 }
-$connection = mysqli_connect('localhost', 'root', '', 'fleet-management');
+$connection = mysqli_connect("begl9q2aqo2yag9pw4jb-mysql.services.clever-cloud.com", "ubeptibrepcuncym", "NGuqOFbgyHyLwhJC67JL", "begl9q2aqo2yag9pw4jb");
 $msg = "";
 if (isset($_POST['submit'])) {
     $regno = $_POST['vehregno'];
