533aa9d3b8e7418c8b7c29593baef29ff079a47d
Bateyjosue <josuebatey19@gmail.com>
2021-11-22 12:23:13 +0200

    configure string connection for deletebill

--- deletebill.php
+++ deletebill.php
@@ -1,14 +1,12 @@
 <?php
-   
-   $id= $_GET['id'];
 
-   $connection=mysqli_connect("localhost","root","","fleet-management");
-   $sql="DELETE FROM bill WHERE bill_id='$id'";
-   $result=mysqli_query($conn,$sql);
-   if(mysqli_query($conn,$sql)){
-       header("Location: indexbill.php");
-   }else{
-         echo "Not deleted";
-   }
-   
-?>
+$id = $_GET['id'];
+
+$connection = mysqli_connect("begl9q2aqo2yag9pw4jb-mysql.services.clever-cloud.com", "ubeptibrepcuncym", "NGuqOFbgyHyLwhJC67JL", "begl9q2aqo2yag9pw4jb");
+$sql = "DELETE FROM bill WHERE bill_id='$id'";
+$result = mysqli_query($conn, $sql);
+if (mysqli_query($conn, $sql)) {
+    header("Location: indexbill.php");
+} else {
+    echo "Not deleted";
+}
