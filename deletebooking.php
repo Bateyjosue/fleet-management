f942ffc639c90e26f094ed1ace223d7ee99f7e57
Bateyjosue <josuebatey19@gmail.com>
2021-11-22 12:23:35 +0200

    configure string connection for deletebooking

--- deletebooking.php
+++ deletebooking.php
@@ -1,16 +1,14 @@
 <?php
-   
-   $id= $_GET['id'];
 
-   $connection=mysqli_connect("localhost","root","","fleet-management");
+$id = $_GET['id'];
 
-   $sql="DELETE FROM `booking` WHERE booking_id='$id'";
-    echo $sql;
-   $result=mysqli_query($conn,$sql);
-   if(mysqli_query($conn,$sql)){
-       header("Location: bookinglist.php");
-   }else{
-         echo "Not deleted";
-   }
-   
-?>
+$connection = mysqli_connect("begl9q2aqo2yag9pw4jb-mysql.services.clever-cloud.com", "ubeptibrepcuncym", "NGuqOFbgyHyLwhJC67JL", "begl9q2aqo2yag9pw4jb");
+
+$sql = "DELETE FROM `booking` WHERE booking_id='$id'";
+echo $sql;
+$result = mysqli_query($conn, $sql);
+if (mysqli_query($conn, $sql)) {
+    header("Location: bookinglist.php");
+} else {
+    echo "Not deleted";
+}
